f134f9c6401b5a72efed16730cd07cac9be02004
Alexander <sacha05bugaev@gmail.com>
2024-04-05 14:20:18 +0300

    add game

---
+++ Game.cs
@@ -0,0 +1,26 @@
+﻿namespace Game;
+
+using Characters;
+
+public class Game
+{
+    private ICharacter player;
+
+    public Game()
+    {
+        player = new Player(1, 1);
+
+    }
+
+    public void GoingLeft(object sender, EventArgs args)
+        => player.MoveLeft();
+
+    public void GoingRight(object sender, EventArgs args)
+    => player.MoveRight();
+
+    public void GoingDown(object sender, EventArgs args)
+    => player.MoveDown();
+
+    public void GoingUp(object sender, EventArgs args)
+    => player.MoveUp();
+}
